73298aff4d02e2b8381dc72525415ef575c64f2a
Euripides <lee82511p@gmail.com>
2023-09-24 15:41:02 +0800

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,110 @@
 <template>
   <div>
-    <h1 class="title">轉(交)換債月交易彙總表</h1>
-    <div class="selected-rows-table">
-      <h2>被選中項目</h2>
-      <button
-        class="clear-button"
-        @click="clearSelectedRows"
-      >
-        清除所有選中項目
-      </button>
-      <table>
+    <input
+      class="search-container"
+      v-model="searchKeyword"
+      placeholder="輸入關鍵字"
+    />
+    <div class="table-container">
+      <table class="custom-table">
         <thead>
           <tr>
-            <th>代號</th>
-            <th>名稱</th>
-            <th>成交量</th>
-            <th>成交金額</th>
-            <th>成交筆數</th>
-            <th>平均</th>
-            <th>最高</th>
-            <th>最低</th>
-            <th>當月最後營業日</th>
-            <th>月份</th>
+            <th @click="sortByColumn(0)">
+              代稱
+              <div
+                class="sort-icon"
+                :class="{
+                  ascending:
+                    sortDirection.column === 0 && sortDirection.ascending,
+                  descending:
+                    sortDirection.column === 0 && !sortDirection.ascending,
+                }"
+              >
+                <span class="arrow-up"></span>
+                <span class="arrow-down"></span>
+              </div>
+            </th>
+            <th @click="sortByColumn(1)">
+              名稱
+              <div
+                class="sort-icon"
+                :class="{
+                  ascending:
+                    sortDirection.column === 1 && sortDirection.ascending,
+                  descending:
+                    sortDirection.column === 1 && !sortDirection.ascending,
+                }"
+              >
+                <span class="arrow-up"></span>
+                <span class="arrow-down"></span>
+              </div>
+            </th>
+            <th @click="sortByColumn(2)">
+              最高
+              <div
+                class="sort-icon"
+                :class="{
+                  ascending:
+                    sortDirection.column === 2 && sortDirection.ascending,
+                  descending:
+                    sortDirection.column === 2 && !sortDirection.ascending,
+                }"
+              >
+                <span class="arrow-up"></span>
+                <span class="arrow-down"></span>
+              </div>
+            </th>
+            <th @click="sortByColumn(3)">
+              最低
+              <div
+                class="sort-icon"
+                :class="{
+                  ascending:
+                    sortDirection.column === 3 && sortDirection.ascending,
+                  descending:
+                    sortDirection.column === 3 && !sortDirection.ascending,
+                }"
+              >
+                <span class="arrow-up"></span>
+                <span class="arrow-down"></span>
+              </div>
+            </th>
+            <th @click="sortByColumn(4)">
+              振幅
+              <div
+                class="sort-icon"
+                :class="{
+                  ascending:
+                    sortDirection.column === 4 && sortDirection.ascending,
+                  descending:
+                    sortDirection.column === 4 && !sortDirection.ascending,
+                }"
+              >
+                <span class="arrow-up"></span>
+                <span class="arrow-down"></span>
+              </div>
+            </th>
           </tr>
         </thead>
+      </table>
+    </div>
+    <div class="table-content">
+      <table class="custom-table">
         <tbody>
           <tr
-            v-for="(row, rowIndex) in selectedRows"
-            :key="rowIndex"
+            v-for="(item, index) in filteredItems"
+            :key="index"
           >
             <td
-              v-for="(cell, cellIndex) in row"
+              v-for="(cell, cellIndex) in item"
               :key="cellIndex"
+              :class="{ highlighted: shouldHighlight(cell) }"
             >
-              {{ cell }}
+              <span v-html="highlightKeyword(cell)"></span>
             </td>
-            <td>{{ selectedRowsMonth[rowIndex] }}</td>
           </tr>
         </tbody>
       </table>
-    </div>
-    <div class="search-container">
-      <input
-        v-model="searchTerm"
-        placeholder="Search"
-      />
-    </div>
-    <div
-      v-for="group in groupedData"
-      :key="group.identifier"
-    >
-      <h2>{{ group.identifier }}</h2>
-      <div class="table-container">
-        <table>
-          <thead class="sticky-thead">
-            <tr>
-              <th>選取</th>
-              <th>代號</th>
-              <th>名稱</th>
-              <th>成交量</th>
-              <th>成交金額</th>
-              <th>成交筆數</th>
-              <th>平均</th>
-              <th>最高</th>
-              <th>最低</th>
-              <th>當月最後營業日</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(row, rowIndex) in filteredData(group.data)"
-              :key="rowIndex"
-            >
-              <td>
-                <input
-                  type="checkbox"
-                  :checked="selectedRows.includes(row)"
-                  @change="toggleRowSelection(row, group.identifier)"
-                />
-              </td>
-              <td
-                v-for="(cell, cellIndex) in row"
-                :key="cellIndex"
-                :class="{ highlighted: shouldHighlight(cell) }"
-              >
-                {{ cell }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
     </div>
   </div>
 </template>
@@ -97,172 +112,204 @@
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import axios from "axios";
+import * as XLSX from "xlsx";
 
-const groupedData = ref([]);
-const searchTerm = ref("");
-const selectedRows = ref([]);
-const selectedRowsMonth = ref([]);
+const items = ref([]);
+const sortDirection = ref({ column: null, ascending: true });
+const searchKeyword = ref("");
 
-onMounted(async () => {
+const fetchData = async () => {
   try {
-    const response = await axios.get("/output.json"); // 使用 Axios 发送 GET 请求
-    const jsonData = response.data; // 获取响应的数据
-
-    if (Array.isArray(jsonData)) {
-      const grouped = groupDataByMonth(jsonData);
-      groupedData.value = grouped;
-    }
+    const response = await axios.get("output.xlsx", {
+      responseType: "arraybuffer",
+    });
+    const arrayBuffer = response.data;
+    const data = parseExcelData(arrayBuffer);
+    items.value = data;
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("讀取資料失敗", error);
   }
-});
+};
 
-function groupDataByMonth(data) {
-  const grouped = [];
+const parseExcelData = (arrayBuffer) => {
+  const data = [];
+  const workbook = XLSX.read(arrayBuffer, { type: "array" });
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-  data.forEach((item) => {
-    const identifier = item.identifier;
-    const existingGroup = grouped.find(
-      (group) => group.identifier === identifier
-    );
+  for (let i = 1; i < rows.length; i++) {
+    const row = rows[i];
+    const thirdColumn = parseFloat(row[2]);
+    const fourthColumn = parseFloat(row[3]);
 
-    if (!existingGroup) {
-      grouped.push({
-        identifier: identifier,
-        headers: item.data[0],
-        data: item.data.slice(1),
-      });
-    } else {
-      existingGroup.data = existingGroup.data.concat(item.data.slice(1));
+    // 計算第五列的值
+    let fifthColumn = (thirdColumn / fourthColumn - 1) * 100;
+
+    // 檢查是否為NaN或Infinity，是的話就顯示0
+    if (isNaN(fifthColumn) || !isFinite(fifthColumn)) {
+      fifthColumn = 0;
     }
-  });
 
-  return grouped;
-}
+    row.push(fifthColumn.toFixed(1) + "%"); // 四捨五入取小數點第一位
 
-const filteredData = computed(() => {
-  return (data) => {
-    if (!searchTerm.value) {
-      return data;
-    }
-    const lowerSearchTerm = searchTerm.value.toLowerCase();
-    return data.filter((row) =>
-      row.some((cell) =>
-        cell.toString().toLowerCase().includes(lowerSearchTerm)
-      )
-    );
-  };
-});
+    data.push(row);
+  }
 
-function shouldHighlight(cell) {
-  if (!searchTerm.value) {
+  return data;
+};
+
+const sortByColumn = (columnIndex) => {
+  const column = columnIndex;
+  const ascending =
+    sortDirection.value.column === column
+      ? !sortDirection.value.ascending
+      : true;
+
+  sortDirection.value = { column, ascending };
+  items.value = bubbleSort(items.value, column, ascending);
+};
+
+const shouldHighlight = (cell) => {
+  if (!searchKeyword.value) {
     return false;
   }
-  const lowerSearchTerm = searchTerm.value.toLowerCase();
+  const lowerSearchTerm = searchKeyword.value.toLowerCase();
   return cell.toString().toLowerCase().includes(lowerSearchTerm);
-}
+};
 
-function toggleRowSelection(row, month) {
-  if (selectedRows.value.includes(row)) {
-    selectedRows.value = selectedRows.value.filter(
-      (selectedRow) => selectedRow !== row
-    );
-    selectedRowsMonth.value = selectedRowsMonth.value.filter(
-      (selectedMonth) => selectedMonth !== month
-    );
-  } else {
-    selectedRows.value.push(row);
-    selectedRowsMonth.value.push(month);
+const highlightKeyword = (cell) => {
+  if (!searchKeyword.value) {
+    return cell; // 如果沒搜索內容不反色
   }
-}
+  const lowerSearchTerm = searchKeyword.value.toLowerCase();
+  const cellText = cell.toString();
+  const startIndex = cellText.toLowerCase().indexOf(lowerSearchTerm);
+  if (startIndex !== -1) {
+    const highlightedText =
+      cellText.substring(0, startIndex) +
+      "<span class='highlight'>" +
+      cellText.substring(startIndex, startIndex + lowerSearchTerm.length) +
+      "</span>" +
+      cellText.substring(startIndex + lowerSearchTerm.length);
+    return highlightedText;
+  } else {
+    return cell; // 都沒匹配顯示全資料
+  }
+};
 
-function clearSelectedRows() {
-  selectedRows.value = [];
-  selectedRowsMonth.value = [];
-}
+const filteredItems = computed(() => {
+  return items.value.filter((item) => {
+    return item.some((cell) =>
+      cell.toString().toLowerCase().includes(searchKeyword.value.toLowerCase())
+    );
+  });
+});
+
+onMounted(() => {
+  fetchData();
+});
+
+const bubbleSort = (arr, column, ascending) => {
+  return arr.slice().sort((a, b) => {
+    const valueA = a[column];
+    const valueB = b[column];
+
+    if (typeof valueA === "number" && typeof valueB === "number") {
+      return ascending ? valueA - valueB : valueB - valueA; // 數字排序
+    } else if (typeof valueA === "string" && typeof valueB === "string") {
+      return ascending
+        ? valueA.localeCompare(valueB)
+        : valueB.localeCompare(valueA); // 字串排序
+    } else {
+      return 0; // 其他類型保持原樣
+    }
+  });
+};
 </script>
 
-<style>
-.title {
-  text-align: center;
+<style scoped>
+.search-container {
+  width: 99.5%;
+  font-size: 30px;
 }
 
-@media screen and (max-width: 768px) {
-  table {
-    font-size: 14px;
-    width: auto;
-  }
-
-  th,
-  td {
-    padding: 6px;
-  }
-
-  .table-container {
-    max-width: 100%;
-    overflow-x: auto;
-  }
+.table-container {
+  max-height: 100vh;
+  overflow-y: auto;
+  top: 0;
+  background-color: white;
 }
 
-.selected-rows-table {
-  margin: 20px auto 20px auto;
-  overflow: auto;
+.table-content {
+  max-height: 85vh;
+  overflow-y: auto;
 }
 
-table {
-  border-collapse: collapse;
+.custom-table {
   width: 100%;
-}
-
-table,
-th,
-td {
+  table-layout: fixed;
+  border-collapse: collapse;
+  margin-top: 20px;
   border: 1px solid #ccc;
 }
 
-th,
-td {
+.custom-table th,
+.custom-table td {
+  border: 1px solid #ccc;
   padding: 8px;
-  text-align: left;
-  white-space: nowrap;
+  text-align: center;
+  word-wrap: break-word;
 }
 
-th {
+.custom-table th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+
+.custom-table tbody tr:nth-child(even) {
   background-color: #f2f2f2;
 }
 
-.search-container {
-  margin-bottom: 10px;
+.sort-icon {
+  display: inline-block;
+  margin-left: 4px;
+  width: 16px;
+  height: 16px;
+  position: relative;
 }
 
-input {
-  width: 100%;
-  padding: 8px;
+.sort-icon .arrow-up,
+.sort-icon .arrow-down {
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-style: solid;
 }
 
-.sticky-thead {
-  position: sticky;
+.sort-icon .arrow-up {
+  border-width: 0 5px 5px 5px;
+  border-color: transparent transparent rgba(0, 0, 0, 0.7) transparent;
   top: 0;
-  background-color: white;
-  z-index: 1;
+}
+
+.sort-icon .arrow-down {
+  border-width: 5px 5px 0 5px;
+  border-color: rgba(0, 0, 0, 0.7) transparent transparent transparent;
+  bottom: 0;
+}
+
+.sort-icon.ascending .arrow-up {
+  border-color: transparent transparent #ff5722 transparent;
+  opacity: 1;
+}
+
+.sort-icon.descending .arrow-down {
+  border-color: #ff5722 transparent transparent transparent;
+  opacity: 1;
 }
 
 .highlighted {
   background-color: rgb(243, 215, 215);
-}
-
-.clear-button {
-  margin-bottom: 20px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  transition: background-color 0.3s ease;
-}
-
-.clear-button:hover {
-  background-color: #0056b3;
+  font-weight: bold;
 }
 </style>
